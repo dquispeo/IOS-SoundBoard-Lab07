8e7c61c03c957e441282cd2cfb5c750e32e0c818
dquispeo <daniel.quispe.o@tecsup.edu.pe>
2020-05-14 15:42:49 -0500

    Initial Commit

---
+++ SoundBoard07/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard07
+//
+//  Created by mbtec22 on 5/14/20.
+//  Copyright © 2020 dquispe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
