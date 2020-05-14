372591e02d0dc51f1e01399e62d25019e9369656
dquispeo <daniel.quispe.o@tecsup.edu.pe>
2020-05-14 18:43:13 -0500

    Solo falta 1 error

---
+++ SoundBoard07/SoundViewController.swift
@@ -0,0 +1,101 @@
+//
+//  SoundViewController.swift
+//  SoundBoard07
+//
+//  Created by mbtec22 on 5/14/20.
+//  Copyright © 2020 dquispe. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import CoreData
+
+class SoundViewController: UIViewController {
+    @IBOutlet weak var recorButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    var audioRecorder : AVAudioRecorder?
+    var audioPlayer : AVAudioPlayer?
+    var audioURL : URL?
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupRecorder()
+        playButton.isEnabled = false
+        addButton.isEnabled = false
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupRecorder(){
+        do{
+            //creando una sesión de audio
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            //Creando una dirección para el archivo de audio
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath,"audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            print("**************************")
+            print(audioURL)
+            print("**************************")
+            //Crear opciones para el grabador de audio
+            var settings : [String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            //Crear el objeto de grabaciones de audio
+            audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder!.prepareToRecord()
+        }catch let error as NSError{
+            print(error)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func recordTapped(_ sender: Any) {
+        if audioRecorder!.isRecording	{
+            //Detener la grabación
+            audioRecorder?.stop()
+            //Cambiar el texto del botón grabar
+            recorButton.setTitle("Record", for: .normal)
+            playButton.isEnabled = true
+            addButton.isEnabled = true
+        }else{
+            //empesar a grabar
+            audioRecorder?.record()
+            //cambiar el título del botón a detener
+            recorButton.setTitle("Stop", for: .normal)
+        }
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        do{
+            try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+            audioPlayer!.play()
+        }catch{}
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        	
+        let sound = Sound(context:context)
+        sound.name = nameTextField.text
+        sound.audio = NSData(contentsOf: audioURL!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    
+}
